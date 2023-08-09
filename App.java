20c6d4fa89c2d74af532baf3f2bc50d884744484
PelayoCubero <141408440+PelayoCubero@users.noreply.github.com>
2023-08-09 15:58:51 +0200

    Add files via upload

--- App.java
+++ App.java
@@ -2,7 +2,9 @@ public class App {
 
     public static void main(String[] args) {
 
-        new Subred(new IP("192.168.1.1", 24), 2);
+    // new Ventana(new Subred(new IP("192.168.1.1", 24), 2));
+
+        new Ventana();
 
     }
 
---
+++ Ventana.java
@@ -0,0 +1,11 @@
+public class Ventana {
+
+    Ventana(Subred s){
+
+    }
+    Ventana(){
+
+
+    }
+
+}
